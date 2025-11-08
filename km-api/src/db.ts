bb713db36bb7540be12c9b241b6275d995939d63
andidev30 <andidev30.personal@gmail.com>
2025-11-09 00:31:59 +0700

    fix wrong env

--- km-api/src/db.ts
+++ km-api/src/db.ts
@@ -1,6 +1,6 @@
 import postgres from "postgres";
 
-const dbHost = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbName = process.env.DB_NAME;
