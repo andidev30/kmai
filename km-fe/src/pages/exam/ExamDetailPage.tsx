b935b14de85cfdf50bef5f9adb443ad5e2c4ecdd
andidev30 <andidev30.personal@gmail.com>
2025-11-09 12:39:09 +0700

    disable button upload

--- km-fe/src/pages/exam/ExamDetailPage.tsx
+++ km-fe/src/pages/exam/ExamDetailPage.tsx
@@ -322,6 +322,7 @@ function ExamDetailPage() {
                         }
                         input.click()
                       }}
+                      disabled={student.status === "graded"}
                     >
                       Upload photo answer
                     </Button>
